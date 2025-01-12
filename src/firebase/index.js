dd6e18d6082693897dcf38b65bd6059fbecdf88a
SivaSabariGanesan <154002297+SivaSabariGanesan@users.noreply.github.com>
2025-01-13 01:27:22 +0530

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,24 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Import getFirestore
-
+import { getFirestore } from "firebase/firestore"; 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXl44QK_R-5J7LEQ-PMMPOPjmobWd-vpE",
-  authDomain: "crud-with-fb-6c73b.firebaseapp.com",
-  projectId: "crud-with-fb-6c73b",
-  storageBucket: "crud-with-fb-6c73b.appspot.com", // Fixed typo in storageBucket
-  messagingSenderId: "568570807459",
-  appId: "1:568570807459:web:153f5fdc1eb79f7db02b16",
+  apiKey: "Your api Key",
+  authDomain: "Your auth domin",
+  projectId: "Your projectid",
+  storageBucket: "Your storagebucket", 
+  messagingSenderId: "Your messagingSenderId",
+  appId: "Your appId",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
-const db = getFirestore(app); // Pass the app instance to getFirestore
+const db = getFirestore(app); \
 
 export { app, db };
